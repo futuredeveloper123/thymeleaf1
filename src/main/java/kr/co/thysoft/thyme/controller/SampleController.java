234a49bdfacf8202a821c60820c4cfc30af76250
DESKTOP-TIIHLUS\TJ_704 <future1develope@kakao.com>
2022-01-15 17:17:55 +0900

    commit1

--- src/main/java/kr/co/thysoft/thyme/controller/SampleController.java
+++ src/main/java/kr/co/thysoft/thyme/controller/SampleController.java
@@ -4,6 +4,9 @@ import kr.co.thysoft.thyme.domain.SampleVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -30,5 +33,27 @@ public class SampleController {
         }
         model.addAttribute("list",list);
         return "ex";
+    }
+    @GetMapping("/inline")
+    public String inline(Model model, HttpSession session
+                         , RedirectAttributes rattr){
+        SampleVO vo = SampleVO.builder()
+                .sno(100L)
+                .first("daj")
+                .last("soft")
+                .regTime(LocalDateTime.now())
+                .build();
+        //모델은 포워딩 할 대 사용
+        model.addAttribute("vo",vo);
+        //이 경우는 세션이 소멸되거나 강제로 삭제할 때까지 유지
+        session.setAttribute("vo1",vo);
+        //한번의 이동에서만 데이터를 유지 - 자동 소멸
+        rattr.addFlashAttribute("vo2",vo);
+        return "redirect:ex1";
+    }
+    @GetMapping("/ex1")
+    public String ex1(){
+
+        return "ex1";
     }
 }
